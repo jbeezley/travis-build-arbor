1114c2ab2fc15123a0fdf871a4cbe23bfccd70af
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-06-08 12:13:47 -0400

    use install.packages

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 set -e -x
-Rscript -e 'install.packages(c("ape","nlme","devtools"), repos=c("http://cran.cnr.Berkeley.edu"))'
-R -e 'options(repos="http://cran.cnr.Berkeley.edu");library(devtools);install_github("diversitree", "richfitz");install_github("geiger-v2", "mwpennell");install_github("cardoonTools", "hafen");install_github("aRbor", "arborworkflows")'
+Rscript -e 'install.packages(c("ape","nlme","devtools", "diversitree", "geiger"), repos=c("http://cran.cnr.Berkeley.edu"))'
+R -e 'options(repos="http://cran.cnr.Berkeley.edu");library(devtools);install_github("cardoonTools", "hafen");install_github("aRbor", "arborworkflows")'
